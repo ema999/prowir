7c392501fa8b76a106a795cddb446d098804da4a
Emanuel Correa <ema.correa1@gmail.com>
2017-10-14 02:10:43 -0300

    -

--- server/app.js
+++ server/app.js
@@ -22,10 +22,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/../client/dist/')));
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/backoffice', express.static(path.join(__dirname, '/backoffice')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
