df6e31536daaff26da861345010df8bc56ce1cd8
Emanuel Correa <ema.correa1@gmail.com>
2017-10-23 16:26:23 -0300

    backoffice login finished

--- client/src/app/main/content/pages/authentication/login/login.component.ts
+++ client/src/app/main/content/pages/authentication/login/login.component.ts
@@ -16,6 +16,7 @@ export class FuseLoginComponent implements OnInit
     loginForm: FormGroup;
     loginFormErrors: any;
     errorCredentials: any = false;
+    isLoading : Boolean = false;
 
     constructor(
         private fuseConfig: FuseConfigService,
@@ -76,13 +77,13 @@ export class FuseLoginComponent implements OnInit
     {
         let email = this.loginForm.controls.email.value;
         let password = this.loginForm.controls.password.value;
+        this.isLoading = true;
+        this.errorCredentials = false;
 
         this.authService.login({email, password}, (err, data) => {
-
+          this.isLoading = false;
           if(err) {this.errorCredentials = err.message; return;}
-          
           if(data.token) return this.router.navigateByUrl('');
-
         });
     }
 }
